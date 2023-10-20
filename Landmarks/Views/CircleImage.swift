b2ae003b4e8062abe57d5035652121b2975b72b3
futaba3 <nyokinyoki41aya@gmail.com>
2023-10-20 23:28:32 +0900

    fix: リストそれぞれのデータを表示するように修正

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -11,12 +11,11 @@ struct CircleImage: View {
     var image: Image
     var body: some View {
         image
-//        Image("turtlerock")
-//            .clipShape(Circle())
-//            .overlay{
-//                Circle().stroke(.white, lineWidth: 4)
-//            }
-//            .shadow(radius: 7)
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
