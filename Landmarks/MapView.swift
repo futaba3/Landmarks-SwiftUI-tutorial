f5877fefb70b3ce8098b4bc7830d80e4fdc2fe6b
futaba3 <nyokinyoki41aya@gmail.com>
2023-10-20 21:37:23 +0900

    Creating and combining views

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,24 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Ayana Kudo on 2023/10/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    // latitudeが緯度longitudeが軽度
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    }
+}
+
+#Preview {
+    MapView()
+}
