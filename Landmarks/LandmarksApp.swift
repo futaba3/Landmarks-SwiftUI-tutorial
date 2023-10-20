e662baf0ce1cc1df9a77fb70e46d7532405d7331
futaba3 <nyokinyoki41aya@gmail.com>
2023-10-20 17:26:01 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ayana Kudo on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
